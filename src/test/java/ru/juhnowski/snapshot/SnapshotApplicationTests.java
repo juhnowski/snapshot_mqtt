994bdf19632a5914de80bbce895761ac782835a7
Juhnowski <juhnowski@gmail.com>
2023-08-03 20:18:19 +0300

    first commit

---
+++ src/test/java/ru/juhnowski/snapshot/SnapshotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.juhnowski.snapshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
