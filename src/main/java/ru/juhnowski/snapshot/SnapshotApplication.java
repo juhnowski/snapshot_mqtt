c7125b3ce3eae42a18a808f1ef254aacea066cf7
Juhnowski <juhnowski@gmail.com>
2023-08-09 15:51:28 +0300

    add docker

--- src/main/java/ru/juhnowski/snapshot/SnapshotApplication.java
+++ src/main/java/ru/juhnowski/snapshot/SnapshotApplication.java
@@ -18,6 +18,8 @@ import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+/// workspace/app
+
 @SpringBootApplication
 public class SnapshotApplication implements CommandLineRunner {
 
@@ -59,19 +61,19 @@ public static String convertDateToString() {
 				String string = new String(payload);
 				//System.out.println("payload: " + string);
 
-				File homeDirectory = new File("/home/ilya/parking/snapshot");
+				File homeDirectory = new File("/workspace/app");
 				String[] envp = new String[1];
 				String[] envp1 = new String[2];
 				//String string = "E121MM750";
 				fn = string+"_"+convertDateToString()+".jpg";
-				envp[0] = "/home/ilya/parking/snapshot/"+fn; //
-				process = Runtime.getRuntime().exec(String.format("ffmpeg -y -loglevel fatal -rtsp_transport tcp -i rtsp://admin:ahH2OoVJLy_@178.140.0.246:1554 -frames:v 2 -r 1 -s 640x480 "+envp[0], envp, homeDirectory));
+				envp[0] = "/workspace/app/"+fn; //
+				process = Runtime.getRuntime().exec(String.format("ffmpeg -y -loglevel fatal -rtsp_transport tcp -i rtsp://admin:ahH2OoVJLy_@192.168.0.246:1554 -frames:v 2 -r 1 -s 640x480 "+envp[0], envp, homeDirectory));
 				process.waitFor();
 				// System.out.println(envp[0]+": done");
 				
 				envp1[0] = "frm/"+string;
 				envp1[1] = fn;
-				process = Runtime.getRuntime().exec("./minio-upload.sh " + envp1[0] + " " + envp1[1],envp1, homeDirectory);
+				process = Runtime.getRuntime().exec("./workspace/app/scripts/minio-upload.sh " + envp1[0] + " " + envp1[1],envp1, homeDirectory);
 				process.waitFor();
 				// System.out.println("=============================================");
 				// System.out.println("/home/ilya/parking/snapshot/minio-upload.sh " + envp1[0] + " " + envp1[1]);
